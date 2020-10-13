45db147164cfe4f1640af6118a0029ac02c3652c
Hamish <hamishdehmer@gmail.com>
2020-10-12 17:03:58 -0700

    Some things.

--- .config/fish/conf.d/omf.fish
+++ .config/fish/conf.d/omf.fish
@@ -5,14 +5,32 @@ set -q XDG_DATA_HOME
 
 # Load Oh My Fish configuration.
 source $OMF_PATH/init.fish
-pfetch
-/home/ziox/color-scripts/color-scripts/bars
-alias pacin="yay -S"
-alias pacup="yay -Syu"
-alias pacre="yay -Rs"
-alias doas="doas --"
-alias matrixme="cmatrix -ksbC magenta -M Arch"
-alias smci="sudo make clean install"
-alias ls='exa -al --color=always --group-directories-first'
+
+# FFF
+export FFF_HIDDEN=1
+export FFF_OPENER="xdg-open"
+export FFF_LS_COLORS=1
+export FFF_STAT_CMD="stat"
+export FFF_CD_ON_EXIT=1
+export FFF_FAV1=~/.config/
+export FFF_FAV2=~/dwm
+export FFF_FAV3=~/pix/Wallpapers/
+export FFF_FAV4=~/dox/
+export FFF_FAV5=/
+export FFF_FAV6=
+export FFF_FAV7=
+export FFF_FAV8=
+export FFF_FAV9=
+export FFF_FILE_FORMAT=" %f"
+export FFF_MARK_FORMAT="> %f*"
+
+# Terminal Factors
+export TERMINAL="kitty"
+export EDITOR="nvim"
+
+# Aliases
 alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME' 
-alias rm="rm -i"
+alias ls='exa -al --color=always --group-directories-first'
+
+pfetch
+~/color-scripts/color-scripts/bars
---
+++ .config/fish/functions/fish_prompt.fish
@@ -0,0 +1,1 @@
+/home/ziox/.local/share/omf/themes/cbjohnson/fish_prompt.fish
